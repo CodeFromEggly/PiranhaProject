74cce62b7c9f69a464125b6ac58d57082991cbfe
CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>
2022-12-19 19:49:16 +0000

    Ping Ping Ping
    go to /poop and press the ping button... have ur discord open

--- app/ping.py
+++ app/ping.py
@@ -2,25 +2,22 @@
 When run, this file will load a discord bot, ping a user, and then shut down.
 I don't want it to run all the time (for the moment, atleast)
 """
-
-print("PING PING PING PING")
-
-
 import discord
+import sys
 
-client = discord.Client()
+intents = discord.Intents.default()
+client = discord.Client(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+    # Ping us in the commands channel (Id: 707990906803978271)::
+    channel = client.get_channel(707990906803978271)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    await channel.send(f"Hey, <@371427291638530060> <@488777165656031232>")
+    await client.close()
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
 
-client.run('MTA1MzM2OTg5ODUxODIwNDQ2Ng.GHNiNv.D3yJqUgl065nZshNJHB1Jh7vchSHOReKWEdyUY')
+client.run('MTA1MzM2OTg5ODUxODIwNDQ2Ng.G6-WUi.oxUInpMtkMGG6nCuG0yNyxe0G7sqIWxYvQfY1k')
+
