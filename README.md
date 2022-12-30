69086a2e4455f6c94d7c1bd7a7644a3eea89f961
sgsmi <109107613+sgsmi@users.noreply.github.com>
2022-12-30 19:49:51 +0000

    Project Summary and minor tweaks
    - Project Summary started
    - Wallet Connect button added, work in progress
    - Logo image now returns user to homepage when clicked
    - 'Active Page' function now updates on reload
    - Index scripts now only implemented on Home page
    
    Co-Authored-By: CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>

--- README.md
+++ README.md
@@ -6,51 +6,94 @@ The above video and following description describe the purpose and creation of C
 Piranha will be a script that will notify us, via email or otherwise, of new listings on OpenSea. It will do this by utilisting OpenSea's Stream API. 
 There will be options for narrowing the script's alerts to specific collections and to within a specific price range.
 
-#### app.py
-app.py renders a front-page which displays featured listings. Its '/conditions' route allows selecting of the script's search terms which are saved in a json file. Currently the conditions form allows selction of:
+#### app.py and templates
+app.py renders a front-page, '/' route, which displays featured listings. The page displays a graphical timeline of NFT 'detections' - OpenSea listings which fell within the filters specified by saved conditions.
+TODO: talk about carousel
+
+
+Its '/conditions' route allows selecting of the script's search terms which are saved in a json file. Currently the conditions form allows selection of:
   - Collection
   - Trading Volume
   - Minimum and Maximum ETH price
   - Margin between listing and WETH offers
   - Number of WETH offers
   - Availability of Sudoswap pool for listing's contract.
+These conditions, or search filters, are saved as search_conditons.JSON so as to be accessed by other files (such as mainstream.js) and be saved for use next time the app is run.
+
+TODO: Talk about layout.html
+
+TODO: Why different routes are followed in app.py
+        Talk about /ping
+  
 
 #### mainstream.js
-mainstream.js will interact with stream-api and filter listings based upon the conditions passed to it.
+mainstream.js will interact with stream-api and filter listings based upon the conditions passed to it. 
+Currently this file serves no pupose as we were unable to aqcuire an API key for the OpenSea marketplace.
 
 
 
 #### piranha.db
-Three tables exist in the database, as follows:
-CREATE TABLE collections(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    slug TEXT NOT NULL,
-    address TEXT NOT NULL,
-    fees INT
-);
+Three tables exist in the database: collections, containing information about NFT collections; keyData, containing data unique to each NFT which needs to be quickly accessed; moreData, containing information which doesn't need to be so readily available. All three tables are linked, using keyData's key and collections' (contract) address. The decision to split NFT information into two tables was so that smaller queries can be made in app.py, and more-succinct Jinja code can be used in buildinh website features such as graph.js.
+Data on SQL implementation is available in /SQL/.
 
 
-CREATE TABLE keyData (
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    token TEXT NOT NULL,
-    collection TEXT NOT NULL,
-    price INT,
-    bestWETH INT,
-    gas INT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(collection) REFERENCES collections(name)
-);
 
+#### UI
 
-CREATE TABLE moreData (
-    id INTEGER PRIMARY KEY,
-    suspicious TEXT,
-    traitFloor INT,
-    rarity INT,
-    image TEXT,
-    sudoswap INT,
-    FOREIGN KEY(id) REFERENCES keyData(key)
-);
+##### STYLING & POSITIONING
 
+###### SCALABILITY
+
+The PIRANHA UI is styled almost entirely with CSS and custom classes/ids. Much of the displayed data is taken from an ever expanding database, so scalability of styling is important.
+
+styles.css contains generic styling for cards, nav and re-usable assets.
+activityStyles.css contains styling specific to activity.html
+
+###### COLOUR SCHEME
+
+For consistency in the UI, the colour scheme is implemented with custom variables defining brand colours and greys of different hues. 
+The following free resources were useful for achieving this:
+
+https://palx.jxnblk.com/ - 'Automatic UI Colour Palette Generator' with a base colour of #36B24A 
+https://color.adobe.com/create/color-wheel - Adobe's colour wheel, used for selecting complimentary brand colours
+https://toolness.github.io/accessible-color-matrix/ - 'Accessible Colour Palette Builder' for ensuring text readability
+
+This method of adding a colour scheme allows for easy modifications to the website design, as well as simple implementation of light/dark mode using javascript.
+
+###### LIGHT/DARK MODE (JavaScript)
+
+The default colour scheme is 'Light Mode' held in :root{} in styles.css, 'Dark Mode' is held as a .dark-mode{} class.
+
+When the page loads - 'Dark Mode', a boolean variable is added to Local Storage and set to false.
+
+When #brightness-button is clicked to activate Dark Mode, the following occurs:
+- .dark-mode is added to the 'HTML' element, 
+- 'Dark Mode' is set to true in local storage
+- The class of #brightness-button is swapped from .bi bi-brightness-high to .bi bi-moon-fill
+- If it is present in the DOM .carousel-dark is added to #cardCarousel
+- The page is reloaded
+
+When clicked again, the reverse occurs:
+- .dark-mode is removed
+- 'Dark Mode' is set to false in local storage
+- Class swapped back
+- .carousel-dark removed from #cardCarousel
+- Page reload
+
+##### NAV
+
+###### DESIGN
+
+The initial design featured a bootstrap navbar however this was too bulky and did not fit with the overall theme. Instead, the nav components have been designed without a framework to match the minimalist style of the rest of the site.
+
+CSS :hover functions are implemented to improve overall UX.
+
+###### ACTIVE PAGE (JavaScript)
+
+**This feature relies on user compliance, requires improvement**
+<sub>Known Bug: Accessing a page via its URL instead of navigation will not switch the .active class</sub>
+
+When first visiting the site:
+- A variable called active-page is set to 'Home', added to Local Storage and the user is redirected to '/'.
+- A javascript function called activeItem() is then called which first removes any present .active class from the nav and adds it to whichever #id matches 'active-page'.
+- 'active-page' is modified when clicking any of the main nav icons - Home, Conditions and Activity using a click event listener on all elements with the class .pNavItem
--- app/static/script.js
+++ app/static/script.js
@@ -1,126 +1,157 @@
-// DARK MODE
+// MAIN SCRIPTS
 
-var htmlElement = document.querySelector('html');
-var brightnessButton = document.querySelector('#brightness-button');
-var darkMode = localStorage.getItem('dark-mode');
-var carousel = document.querySelector('.carousel');
+    // DARK MODE
 
-if (darkMode === 'true') {
-    htmlElement.classList.add('dark-mode');
-    brightnessButton.setAttribute('class', 'bi bi-moon-fill');
-    if(carousel) {
-        carousel.classList.remove("carousel-dark");
-    }
-}
+        var htmlElement = document.querySelector('html');
+        var brightnessButton = document.querySelector('#brightness-button');
+        var darkMode = localStorage.getItem('dark-mode');
+        var carousel = document.querySelector('.carousel');
 
-brightnessButton.addEventListener('click', function() {
-    if (htmlElement.classList.contains('dark-mode')) {
-        htmlElement.classList.remove('dark-mode');
-        if(carousel)
-        {
-            carousel.classList.add("carousel-dark");
+        if (darkMode === 'true') {
+            htmlElement.classList.add('dark-mode');
+            brightnessButton.setAttribute('class', 'bi bi-moon-fill');
+            if(carousel) {
+                carousel.classList.remove("carousel-dark");
+            }
         }
-        brightnessButton.setAttribute('class', 'bi bi-brightness-high');
-        localStorage.removeItem('dark-mode');
-    } else {
-        htmlElement.classList.add('dark-mode');
-        brightnessButton.setAttribute('class', 'bi bi-moon-fill');
-        localStorage.setItem('dark-mode', htmlElement.classList.contains('dark-mode'));
-        if(carousel)
-        {
-            carousel.classList.remove("carousel-dark");
+
+        brightnessButton.addEventListener('click', function() {
+            if (htmlElement.classList.contains('dark-mode')) {
+                htmlElement.classList.remove('dark-mode');
+                if(carousel)
+                {
+                    carousel.classList.add("carousel-dark");
+                }
+                brightnessButton.setAttribute('class', 'bi bi-brightness-high-fill');
+                localStorage.removeItem('dark-mode');
+            } else {
+                htmlElement.classList.add('dark-mode');
+                brightnessButton.setAttribute('class', 'bi bi-moon-fill');
+                localStorage.setItem('dark-mode', htmlElement.classList.contains('dark-mode'));
+                if(carousel)
+                {
+                    carousel.classList.remove("carousel-dark");
+                }
+            }
+            location.assign(location.href);
+        });
+
+
+    // ACTIVE NAV SELECTOR
+
+        // Get the current page path
+        var activePage = window.location.pathname;
+
+        // Remove the '/' character from the activePage string
+        activePage = activePage.replace('/', '');
+
+        // Set the active page in local storage
+        if (activePage === '') {
+            activePage = 'home';
         }
-    }
-    location.assign(location.href);
-});
+
+        localStorage.setItem('active-page', activePage);
+
+        const list = document.querySelectorAll('.pNavItem');
+        function activeItem() {
+            list.forEach((item) =>
+            item.classList.remove('active'));
+            this.classList.add('active');
+            localStorage.setItem('active-page', this.firstElementChild.id);
+        }
+
+        document.getElementById(activePage).classList.add('active');
+        list.forEach((item) =>
+        item.addEventListener('click', activeItem));
 
 
-// ACTIVE NAV SELECTOR
+    // WALLET CONNECT BUTTON
 
-var activePage = localStorage.getItem('active-page');
+        document.getElementById("connect-button").addEventListener("click", function() {
+            // Show the floating div
+            document.getElementById("connect-div").style.display = "block";
+        
+            // Grey out the rest of the page
+            document.getElementById("overlay").style.display = "block";
 
-if (activePage === null) {
-    localStorage.setItem('active-page', 'Home');
-}
-
-const list = document.querySelectorAll('.pNavItem');
-function activeItem() {
-    list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
-    localStorage.setItem('active-page', this.firstElementChild.id);
-}
-
-document.getElementById(activePage).classList.add('active');
-list.forEach((item) =>
-item.addEventListener('click', activeItem));
+            var exitButton = document.getElementById("connect-exit");
+            exitButton.addEventListener('click', function() {
+                document.getElementById('connect-div').style.display = 'none'; document.getElementById('overlay').style.display = 'none';
+            });
+        });
 
 
-// FEATURED CARD DISPLAY
+// INDEX SCRIPTS
 
-$(document).ready(function(){
-    $("#featuredCard").css("display", "none");
-    $(".miniCard").click(function(){
-        if($(this).hasClass("selectedCard")) {
-            $(this).removeClass("selectedCard");
+    if (activePage === 'home') {
+
+    // FEATURED CARD DISPLAY
+
+        $(document).ready(function(){
             $("#featuredCard").css("display", "none");
-            $("#emptyCard").css("display", "block");
-        } else {
-            $(".selectedCard").removeClass("selectedCard");
-            $(this).addClass("selectedCard");
-            var selectedCardImgSrc = $(this).find("img").attr("src");
-            var selectedCardTitle = $(this).find(".card-title").html();
-            var selectedCardOsLink = $(this).find("a.osLink").attr("href");
+            $(".miniCard").click(function(){
+                if($(this).hasClass("selectedCard")) {
+                    $(this).removeClass("selectedCard");
+                    $("#featuredCard").css("display", "none");
+                    $("#emptyCard").css("display", "block");
+                } else {
+                    $(".selectedCard").removeClass("selectedCard");
+                    $(this).addClass("selectedCard");
+                    var selectedCardImgSrc = $(this).find("img").attr("src");
+                    var selectedCardTitle = $(this).find(".card-title").html();
+                    var selectedCardOsLink = $(this).find("a.osLink").attr("href");
 
-            $("#featuredCard").find("img").attr("src", selectedCardImgSrc);
-            $("#featuredCard").find(".card-title").html(selectedCardTitle);
-            $("#featuredCard").find("a.osLink").attr("href", selectedCardOsLink);
-            $("#emptyCard").css("display", "none");
-            $("#featuredCard").css("display", "block");
-        }
-    });
-    
-    // Add event.stopPropagation() method call here
-    $(".button-group").click(function(event) {
-        event.stopPropagation();
-    });
-}); 
+                    $("#featuredCard").find("img").attr("src", selectedCardImgSrc);
+                    $("#featuredCard").find(".card-title").html(selectedCardTitle);
+                    $("#featuredCard").find("a.osLink").attr("href", selectedCardOsLink);
+                    $("#emptyCard").css("display", "none");
+                    $("#featuredCard").css("display", "block");
+                }
+            });
+            
+            // Add event.stopPropagation() method call here
+            $(".button-group").click(function(event) {
+                event.stopPropagation();
+            });
+        }); 
 
 
-// SLIDERS
+    // SLIDERS
 
-const profitMarginMinInput = document.getElementById('profitMarginMin');
-const profitMarginMaxInput = document.getElementById('profitMarginMax');
-const profitMarginMinValue = document.getElementById('profitMarginMinValue');
-const profitMarginMaxValue = document.getElementById('profitMarginMaxValue');
+        const profitMarginMinInput = document.getElementById('profitMarginMin');
+        const profitMarginMaxInput = document.getElementById('profitMarginMax');
+        const profitMarginMinValue = document.getElementById('profitMarginMinValue');
+        const profitMarginMaxValue = document.getElementById('profitMarginMaxValue');
 
-profitMarginMinInput.addEventListener('input', () => {
-  profitMarginMaxInput.min = profitMarginMinInput.value;
-  profitMarginMinValue.textContent = profitMarginMinInput.value;
-});
+        profitMarginMinInput.addEventListener('input', () => {
+        profitMarginMaxInput.min = profitMarginMinInput.value;
+        profitMarginMinValue.textContent = profitMarginMinInput.value;
+        });
 
-profitMarginMaxInput.addEventListener('input', () => {
-    profitMarginMinInput.max = profitMarginMaxInput.value;
-    profitMarginMaxValue.textContent = profitMarginMaxInput.value;
-});
+        profitMarginMaxInput.addEventListener('input', () => {
+            profitMarginMinInput.max = profitMarginMaxInput.value;
+            profitMarginMaxValue.textContent = profitMarginMaxInput.value;
+        });
 
-profitMarginMinValue.textContent = profitMarginMinInput.value;
-profitMarginMaxValue.textContent = profitMarginMaxInput.value;
+        profitMarginMinValue.textContent = profitMarginMinInput.value;
+        profitMarginMaxValue.textContent = profitMarginMaxInput.value;
 
-const priceRangeMinInput = document.getElementById('priceRangeMin');
-const priceRangeMaxInput = document.getElementById('priceRangeMax');
-const priceRangeMinValue = document.getElementById('priceRangeMinValue');
-const priceRangeMaxValue = document.getElementById('priceRangeMaxValue');
+        const priceRangeMinInput = document.getElementById('priceRangeMin');
+        const priceRangeMaxInput = document.getElementById('priceRangeMax');
+        const priceRangeMinValue = document.getElementById('priceRangeMinValue');
+        const priceRangeMaxValue = document.getElementById('priceRangeMaxValue');
 
-priceRangeMinInput.addEventListener('input', () => {
-    priceRangeMaxInput.min = priceRangeMinInput.value;
-    priceRangeMinValue.textContent = priceRangeMinInput.value;
-});
-  
-priceRangeMaxInput.addEventListener('input', () => {
-      priceRangeMinInput.max = priceRangeMaxInput.value;
-      priceRangeMaxValue.textContent = priceRangeMaxInput.value;
-});
+        priceRangeMinInput.addEventListener('input', () => {
+            priceRangeMaxInput.min = priceRangeMinInput.value;
+            priceRangeMinValue.textContent = priceRangeMinInput.value;
+        });
+        
+        priceRangeMaxInput.addEventListener('input', () => {
+            priceRangeMinInput.max = priceRangeMaxInput.value;
+            priceRangeMaxValue.textContent = priceRangeMaxInput.value;
+        });
 
-priceRangeMinValue.textContent = priceRangeMinInput.value;
-priceRangeMaxValue.textContent = priceRangeMaxInput.value;
+        priceRangeMinValue.textContent = priceRangeMinInput.value;
+        priceRangeMaxValue.textContent = priceRangeMaxInput.value;
+        
+    }
